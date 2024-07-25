639bda84320f4067e19a3dd8ecd56cf3b50890dc
Tyrrell Dakin <tdakin4@nait.ca>
2024-07-24 21:46:25 -0600

    Struggled on mobile layout

--- js/main.js
+++ js/main.js
@@ -1,1 +1,21 @@
+const menu = document.querySelector(".menu-icon");
+const mainMenu = document.querySelector(".main-menu");
+const category = document.querySelector(".category");
 
+menu.addEventListener("click", displayMenu);
+
+function displayMenu() {
+    mainMenu.classList.toggle("show-menu");
+    if (mainMenu.classList.contains("show-menu")) {
+        menu.style.display = "none";
+    }   else {
+        menu.style.display = "block";
+    }
+}
+
+mainMenu.addEventListener("click", displayJoystick );
+
+function displayJoystick() {
+    menu.style.display = "block";
+    mainMenu.classList.toggle("show-menu");
+}
